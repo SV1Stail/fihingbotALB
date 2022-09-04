0400016b32bba236ba87a019d3cc4f3ae3dc3e58
SV1Stail <vprs6900@mail.ru>
2022-09-04 21:10:45 +0300

    закончил 2 урок

--- image.py
+++ image.py
@@ -1,8 +1,13 @@
 import cv2
+import numpy as np
 
 img= cv2.imread("images/fishing.jpg")
-img=cv2.resize(img,(img.shape[1]//3,img.shape[0]//3))
+img=cv2.resize(img,(img.shape[1]//2,img.shape[0]//2))
 img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img=cv2.Canny(img,50,50)
+matritsa=np.ones((5,5), np.uint0)
+
+
 cv2.imshow("fishing",img)
 
 
