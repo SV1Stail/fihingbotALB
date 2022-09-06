226d9387bbb7bf30a71f74192ad5e6ae469b40d0
SV1Stail <vprs6900@mail.ru>
2022-09-06 19:56:56 +0300

    разделение на слои

--- photosplit.py
+++ photosplit.py
@@ -3,12 +3,12 @@ np=libraries.np
 cv2=libraries.cv2
 
 img = cv2.imread("images/fishing.jpg")
+img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+b = cv2.split(img) #разделения на слоя
 
-b,g,r = cv2.split(img)
 
 
-
-img = cv2.merge([r,b,g])
+#img = cv2.merge([r,b,g])
 cv2.imshow("result",img)
 cv2.waitKey(0)
 
